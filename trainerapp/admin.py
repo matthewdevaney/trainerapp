4a6c24bed20d76d81aa060f31814151a82bc218a
mdevaney <mbdevaney@gmail.com>
2018-08-12 19:08:59 -0500

    customize admin panel

--- trainerapp/admin.py
+++ trainerapp/admin.py
@@ -4,20 +4,24 @@ from .models import Attendee, Course, Event, Student
 
 @admin.register(Attendee)
 class AdminAttendee(admin.ModelAdmin):
+    list_display = ['id', 'event', 'student', 'attended']
     fields = ['event_id', 'course_id', 'attended']
 
 
 @admin.register(Course)
 class AdminCourse(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description', 'hours', 'inactive']
     fields = ['title', 'description', 'hours', 'inactive']
 
 
 @admin.register(Event)
 class AdminEvent(admin.ModelAdmin):
-    fields = ['course_id', ('start_datetime', 'end_datetime'), ('location', 'capacity')]
+    list_display = ['id', 'course_id', 'location', 'capacity', 'start_datetime', 'end_datetime']
+    fields = ['course', ('location', 'capacity'), ('start_datetime', 'end_datetime')]
 
 
 @admin.register(Student)
 class AdminStudent(admin.ModelAdmin):
+    list_display = ['id', 'first_name', 'last_name', 'email', 'inactive']
     fields = ['first_name', 'last_name', 'email', 'inactive']
 
